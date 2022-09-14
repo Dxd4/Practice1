412e29918c85ef3b709a22a0436766047b0c8193
Dxd4 <46905365+Dxd4@users.noreply.github.com>
2022-09-14 19:58:56 +0300

    Add files via upload

--- Practice1/Shapes/Circle.java
+++ Practice1/Shapes/Circle.java
@@ -1,10 +1,13 @@
 package Practice1.Shapes;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class Circle extends Shape {
 	private int x;
 	private int y;
 
-	public Circle(String color, int x, int y) {
+	public Circle(Color color, int x, int y) {
 		super(color);
 		this.x = x;
 		this.y = y;
@@ -28,7 +31,29 @@ public class Circle extends Shape {
 
 	@Override
 	public void draw() {
-		System.out.printf("Circle color: %s, x: %d, y: %d\n", this.getColor(), this.getx(), this.gety());
+		System.out.printf("Circle color: %s, x: %d, y: %d\n", this.getColor().toString(), this.getx(), this.gety());
+		// creating object of JFrame(Window popup)
+		JFrame window = new JFrame();
+
+		// setting size of the pop window
+		window.setBounds(30, 30, getx()+50, gety()+50);
+
+		// setting canvas for draw
+		window.getContentPane().add(new MyCanvas());
+
+		// set visibility
+		window.setVisible(true);
+
+	}
+
+	class MyCanvas extends JComponent {
+
+		public void paint(Graphics g)
+		{
+			g.setColor(getColor());
+			g.drawOval(10,10,getx(),gety());
+			g.fillOval(10,10,getx(),gety());
+		}
 	}
 
 	@Override
--- Practice1/Shapes/Main.java
+++ Practice1/Shapes/Main.java
@@ -1,10 +1,12 @@
 package Practice1.Shapes;
 
+import java.awt.*;
+
 public class Main {
 	public static void main(String[] args) {
 		Shape[] shapes = new Shape[2];
-		shapes[0] = new Circle("red",1,2);
-		shapes[1] = new Rectangle("blue", 1,2,3,4);
+		shapes[0] = new Circle(Color.RED,100,100);
+		shapes[1] = new Rectangle(Color.BLUE, 10,10,100,100);
 
 		for (Shape shape : shapes) {
 			shape.draw();
