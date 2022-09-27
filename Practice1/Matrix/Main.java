bebe04f590029cf0cf6ec7e93846d9e084159321
Dxd4 <46905365+Dxd4@users.noreply.github.com>
2022-09-27 20:24:39 +0000

    Update Main.java

--- Practice1/Matrix/Main.java
+++ Practice1/Matrix/Main.java
@@ -2,6 +2,7 @@ package Practice1.Matrix;
 
 public class Main {
 	public static void main(String[] args) {
+		//Hello from Dxd4
 		Matrix matrix = new Matrix(2);
 		matrix.showMatrix();
 		matrix.numberMultiply(5);
