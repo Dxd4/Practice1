fa9460e7a57ee3e9d84543030f515ba9d1a44c76
Dxd4 <46905365+Dxd4@users.noreply.github.com>
2022-09-27 20:24:55 +0000

    Update Main.java

--- Practice1/Shapes/Main.java
+++ Practice1/Shapes/Main.java
@@ -4,6 +4,7 @@ import java.awt.*;
 
 public class Main {
 	public static void main(String[] args) {
+		//Hello from Dxd4
 		Shape[] shapes = new Shape[2];
 		shapes[0] = new Circle(Color.RED,100,100);
 		shapes[1] = new Rectangle(Color.BLUE, 10,10,100,100);
